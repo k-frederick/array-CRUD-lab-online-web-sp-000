fb22eb176e36efcf138c04f24c99705b3b5e21b5
Kristina Frederick <kristinafrederick@icloud.com>
2020-05-13 23:37:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,12 +17,12 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["arrays!"]
+  array = ["popsicles", "arrays!"]
   arrays = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+  array = []
 end
 
 def retrieve_element_from_index(array, index_number)
