73d21ef0bcc6be28ac8959ca429b09465481894f
Kristina Frederick <kristinafrederick@icloud.com>
2020-05-13 23:29:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  Tolkein = ["Fellowship", "Two Towers", "Return of the King", "Hobbit"]
+  ["Fellowship", "Two Towers", "Return of the King", "Hobbit"]
 end
 
 def add_element_to_end_of_array(array, element)
